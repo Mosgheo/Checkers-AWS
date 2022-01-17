031d32dc8a6656c3309458c0d741301a1dde7e98
Mosgheo <luca.salvigni7@studio.unibo.it>
2022-01-17 15:14:48 +0100

    Add pagination for Leaderboard and history games

--- frontend/Checkers/src/components/profileComponents/MatchInfo.vue
+++ frontend/Checkers/src/components/profileComponents/MatchInfo.vue
@@ -12,9 +12,9 @@
     </thead>
 
     <tbody>
-      <template v-for="(user, i) in this.history" :key="i">
+      <template v-for="(user, i) in this.currentPage" :key="i">
         <tr>
-          <th :textContent="'#' + (i+1)"></th>
+          <th :textContent="'#' + ((this.history.indexOf(this.currentPage[i]))+1) "></th>
           <td>
             <div class="flex items-center space-x-3">
               <div class="avatar">
@@ -55,6 +55,16 @@
     </tbody>
 
   </table>
+
+  <div v-if="this.history.length > this.perPage" class="btn-group mt-4">
+    <button @click="previousPage($event)" class="btn btn-disabled btn-outline btn-wide">Previous</button> 
+    <button @click="nextPage($event)" class="btn btn-outline btn-wide">Next</button>
+  </div>
+  <div v-else class="btn-group mt-4">
+    <button @click="previousPage($event)" class="btn btn-disabled btn-outline btn-wide">Previous</button> 
+    <button @click="nextPage($event)" class="btn btn-disabled btn-outline btn-wide">Next</button>
+  </div>
+
 </div>
 </template>
 
@@ -68,12 +78,57 @@ export default {
     api.get_history(this.$socket)
 
     return {
-      history: null
+      history: [],
+      currentPage: [],
+      perPage: 7,
+      page: 1
+    }
+  },
+  methods: {
+    nextPage(button) {
+      this.currentPage = []
+      for(let i = (this.perPage*this.page); i < (this.page + 1)*this.perPage; i++) {
+        if(this.history[i] === undefined) {
+          break;
+        }
+        this.currentPage.push(this.history[i])
+      }
+      this.page++
+      if(this.history.at(-1) === this.currentPage.at(-1)) {
+        button.path[1].children[1].setAttribute("class", "btn btn-disabled btn-outline btn-wide")
+      }
+      if(button.path[1].children[0].className.includes("disabled")) {
+        button.path[1].children[0].setAttribute("class", "btn btn-outline btn-wide")
+      }
+    },
+    previousPage(button) {
+      this.currentPage = []
+      var fillTo = ((this.perPage*this.page)-1) - this.perPage
+      for(let i = (fillTo - this.perPage)+1; i <= fillTo; i++) {
+        if(this.history[i] === undefined) {
+          break;
+        }
+        this.currentPage.push(this.history[i])
+      }
+      this.page--
+      if(this.history[0] === this.currentPage[0]) {
+        button.path[1].children[0].setAttribute("class", "btn btn-disabled btn-outline btn-wide")
+      }
+      if(button.path[1].children[1].className.includes("disabled")) {
+        button.path[1].children[1].setAttribute("class", "btn btn-outline btn-wide")
+      }
     }
   },
   sockets: {
     user_history(res) {
       this.history = res
+      console.log(this.history)
+      for(let i = 0; i < this.perPage; i++) {
+        if(this.history[i] === undefined) {
+          break;
+        }
+        this.currentPage.push(this.history[i])
+      }
     }
   }
 }
